2643b64959d5e8ebf45f8809cb6378604ae18805
Hatim bin Ashraf <hatim.ashraf91@gmail.com>
2025-03-16 03:38:26 +0100

    react-router-setting

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,31 @@
+import { Home } from './pages/Home';
+import { Country } from './pages/Country';
+import { Contact } from './pages/Contact';
+import { About } from './pages/About';
 import './App.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className=' p-5 flex flex-col items-center h-screen gap-12'>
-      <h1 className='text-3xl font-bold underline'>Hello</h1>
-      <h1 className='text-3xl font-bold underline'>Hello</h1>
-    </div>
-  );
-}
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+  },
+  {
+    path: 'country',
+    element: <Country />,
+  },
+  {
+    path: 'contact',
+    element: <Contact />,
+  },
+  {
+    path: 'about',
+    element: <About />,
+  },
+]);
+
+const App = () => {
+  return <RouterProvider router={router}></RouterProvider>;
+};
 
 export default App;
